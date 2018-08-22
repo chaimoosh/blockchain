df7aac0810b6721c9f7bcc9bd7b3cbf52c96d147
Chaim Reisman <chaimoosh@gmail.com>
2018-08-22 16:08:02 -0400

    Working

--- index.js
+++ index.js
@@ -32,6 +32,23 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1]
+
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false
+      }
+
+      return true
+    }
+  }
 }
 
 
@@ -42,4 +59,8 @@ coin.addBlock(new Block(3, "05/13/2018", { amount: 5 }))
 coin.addBlock(new Block(4, "05/09/2018", { amount: 67 }))
 coin.addBlock(new Block(5, "07/13/2018", { amount: 56 }))
 
-console.log(JSON.stringify(coin, null, 4))
+console.log('Is blockchain valid?' + coin.isChainValid())
+//console.log(JSON.stringify(coin, null, 4))
+coin.chain[2].data = { amount: 546 }
+//coin.chain[2].hash = coin.chain[2].calculateHash()
+console.log('Is blockchain valid?' + coin.isChainValid())
